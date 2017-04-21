d22ff8622d5850b8bbac14c7e61143407d35ff72
Lee Dohm <lee@lee-dohm.com>
2017-04-20 17:29:30 -0700

    Add tests for nullable columns in modules

--- test/models/module_test.exs
+++ test/models/module_test.exs
@@ -8,11 +8,31 @@ defmodule ShipDesigner.ModuleTest do
 
   test "changeset with valid attributes" do
     changeset = Module.changeset(%Module{}, @valid_attrs)
+
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
     changeset = Module.changeset(%Module{}, @invalid_attrs)
+
     refute changeset.valid?
+  end
+
+  test "allows nil mass" do
+    changeset = Module.changeset(%Module{}, %{@valid_attrs | mass: nil})
+
+    assert changeset.valid?
+  end
+
+  test "allows nil price" do
+    changeset = Module.changeset(%Module{}, %{@valid_attrs | price: nil})
+
+    assert changeset.valid?
+  end
+
+  test "allows nil power_draw" do
+    changeset = Module.changeset(%Module{}, %{@valid_attrs | power_draw: nil})
+
+    assert changeset.valid?
   end
 end
