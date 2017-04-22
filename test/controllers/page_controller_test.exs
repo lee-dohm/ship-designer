3962ec2b11a9c076364c71a8a09314ce0eb86866
Lee Dohm <lee@lee-dohm.com>
2017-04-22 09:07:29 -0700

    Fix broken tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule ShipDesigner.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+
+    assert html_response(conn, 200) =~ "Welcome to"
   end
 end
