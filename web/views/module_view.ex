ea16fe108bce38a95d48af30454ef3bd8fbf7231
Lee Dohm <lee@lee-dohm.com>
2017-04-21 18:59:34 -0700

    Pull formatting up into the view module

--- web/views/module_view.ex
+++ web/views/module_view.ex
@@ -2,4 +2,13 @@ defmodule ShipDesigner.ModuleView do
   use ShipDesigner.Web, :view
 
   import Number.Delimit
+
+  def format_price(module), do: Number.Delimit.number_to_delimited(module.price)
+
+  def format_size(module) do
+    cond do
+      module.category == "Bulkhead" -> nil
+      true -> "#{module.class}#{module.rating}"
+    end
+  end
 end
