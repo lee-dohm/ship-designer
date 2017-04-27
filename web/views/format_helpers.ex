0b8c0236a24354a91e1a0e8eb1a2ffc358508ac6
Lee Dohm <lee@liftedstudios.com>
2017-04-27 15:16:37 -0700

    Add format_speed formatter

--- web/views/format_helpers.ex
+++ web/views/format_helpers.ex
@@ -8,34 +8,37 @@ defmodule ShipDesigner.FormatHelpers do
 
   ## Options
 
-  * `:with_unit` - Displays the distance with the unit designation
+  * `:with_unit` - Displays the distance with the given unit designation (defaults to `km`)
   """
   def format_distance(distance, options \\ %{})
 
   def format_distance(distance, %{}), do: "#{distance}"
+  def format_distance(distance, :with_unit), do: format_distance(distance, %{with_unit: "km"})
 
-  def format_distance(distance, :with_unit) do
+  def format_distance(distance, with_unit: unit) do
     case distance do
-      nil -> "0 km"
-      _ -> "#{distance} km"
+      nil -> "0 #{unit}"
+      _ -> "#{distance} #{unit}"
     end
   end
+
 
   @doc """
   Formats an amount of mass for display.
 
   ## Options
 
-  * `:with_unit` - Displays the mass with the unit designation
+  * `:with_unit` - Displays the mass with the given unit designation (defaults to `T`)
   """
   def format_mass(mass, options \\ %{})
 
   def format_mass(mass, %{}), do: "#{mass}"
+  def format_mass(mass, :with_unit), do: format_mass(mass, with_unit: "T")
 
-  def format_mass(mass, :with_unit) do
+  def format_mass(mass, with_unit: unit) do
     case mass do
-      nil -> "0 T"
-      _ -> "#{mass} T"
+      nil -> "0 #{unit}"
+      _ -> "#{mass} #{unit}"
     end
   end
 
@@ -72,6 +75,25 @@ defmodule ShipDesigner.FormatHelpers do
     case power do
       nil -> "0 MW"
       _ -> "#{power} MW"
+    end
+  end
+
+  @doc """
+  Formats a speed for display.
+
+  ## Options
+
+  * `:with_unit` - Displays the speed with the unit designation (defaults to `m/s`)
+  """
+  def format_speed(speed, options \\ %{})
+
+  def format_speed(speed, %{}), do: "#{speed}"
+  def format_speed(speed, :with_unit), do: format_speed(speed, with_unit: "m/s")
+
+  def format_speed(speed, with_unit: unit) do
+    case speed do
+      nil -> "0 #{unit}"
+      _ -> "#{speed} #{unit}"
     end
   end
 end
